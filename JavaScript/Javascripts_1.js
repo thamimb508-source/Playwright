e1690018ffd39c66701607ed7e340ff48b8725ab
thamimb508-source <thamimb508@gmail.com>
2026-04-08 16:38:17 -0700

    Filesupdate

---
+++ JavaScript/Javascripts_1.js
@@ -0,0 +1,44 @@
+// datatype ---> how to define a data
+// type 1.primitive datatype ---> String, num, boolean, null, undefined, big init
+
+//non primitive datatype
+
+//string
+let name = "Javascript"
+console.log(name);
+
+//num
+let a=12;
+console.log(a);
+let b=12.123;
+console.log(b);
+
+// bollean
+
+let c=true;
+let d=false;
+
+// null
+
+let t=null;
+
+// undefined
+
+let z;
+console.log(z);
+
+// bigint // you can store n number of numeric values,
+// in last you can put the n for reference to n number values
+
+let bigint = 234567845678945678956789n;
+
+// non primitive datatype
+// obj
+
+
+
+
+
+
+
+
