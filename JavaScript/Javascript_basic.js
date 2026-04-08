35731009ab42847c28f37f539e9772cac2f02d54
thamimb508-source <thamimb508@gmail.com>
2026-04-07 21:53:29 -0700

    File added

---
+++ JavaScript/Javascript_basic.js
@@ -0,0 +1,25 @@
+var a=10; //Declare
+console.log(a);
+var b=20; //Re-declare
+console.log(b);
+var x;
+x=30; //re-assign
+console.log(x);
+
+
+//.let --> block scope
+
+//let a=40;//delare
+//console.log(a);
+
+//let a=50; // you cannot redeclare using let
+
+let c; // Re assign
+c=60;
+console.log(c);
+
+//3. .const --block scope
+const country= "India" // declare
+console.log(country);
+
+//const country = "usa"; // you cannot re-declare and reassign using const
